70c54b66b29cf75e35d1a5a9e401bc879964bd3e
Wedson Pereira de Souza <wedson.souza@squadra.com.br>
2025-11-21 18:25:12 -0300

    Novo ajuste Login

--- Cadastramento.Infrastructure/IRedisSessionService.cs
+++ Cadastramento.Infrastructure/IRedisSessionService.cs
@@ -7,5 +7,6 @@ namespace Cadastramento.Infrastructure
     {
         Task<bool> SessionExistsAsync(string sessionId);
         Task<Dictionary<string, string>> GetSessionHashAsync(string sessionId);
+        Task SaveSessionAsync(string sessionId, Dictionary<string, string> values, int ttlSeconds = 3600);
     }
 }
